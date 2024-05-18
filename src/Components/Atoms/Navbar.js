094dbe57c40c1484f2bde0958d969f685540ab56
EmadNejati <emadnejati97@gmail.com>
2024-05-18 14:34:53 +0330

    Hover Card Shadow Style

--- src/Components/Atoms/Navbar.js
+++ src/Components/Atoms/Navbar.js
@@ -3,7 +3,7 @@ import "./Navbar.css"
 function Navbar({ countryList }) {
     return (
         <div className="navbar__container flex flex-row list-none w-full justify-center items-center">
-            {countryList.map((country, index) => <li className="navbar__item w-1/6 h-auto mx-2 font-semibold text-md lg:text-xl flex justify-center items-center">{country}</li>
+            {countryList.map((country, index) => <li key={index} className="navbar__item w-1/6 h-auto mx-2 font-semibold text-md lg:text-xl flex justify-center items-center">{country}</li>
             )}
         </div>
     );
--- src/Components/Atoms/ProductCard.js
+++ src/Components/Atoms/ProductCard.js
@@ -2,19 +2,30 @@ import { useState } from "react";
 import "./ProductCard.css"
 function ProductCard({ productDsc, productImgUrl }) {
 
-    const [captionStyle,setCaptionStyle]=useState("card__dsc text-white rounded-2xl p-5 flex flex-row justify-start items-end w-full h-full mt-20 z-50  text-2xl font-bold text-my_white ")
-    
-    const hoverCard=(status)=>{
-        
-        status===true?setCaptionStyle("card__dsc text-white rounded-2xl p-5 flex flex-row justify-start items-end w-full h-full mt-20 z-50  text-2xl font-bold text-my_dark_gray"):setCaptionStyle("card__dsc text-white rounded-2xl p-5 flex flex-row justify-start items-end w-full h-full mt-20 z-50  text-2xl font-bold text-my_white")
+    const [captionStyle, setCaptionStyle] = useState("card__dsc text-white rounded-2xl p-5 flex flex-row justify-start items-end w-full h-full mt-20 z-50  text-2xl font-bold text-my_white ")
+
+    const [cardStyle, setCardStyle] = useState("product__card w-full h-auto rounded-3xl flex flex-col items-center justify-center bg-orange-900 relative my-3 mx-3 ")
+
+    const hoverCard = (status) => {
+
+        if (status === true) {
+            setCaptionStyle("card__dsc  text-white rounded-2xl p-5 flex flex-row justify-start items-end w-full h-full mt-20 z-50  text-2xl font-bold text-my_dark_gray ");
+
+            setCardStyle("product__card w-full h-auto rounded-3xl flex flex-col items-center justify-center bg-orange-900 relative my-3 mx-3 border-1 border-my_dark_gray shadow-2xl ");
+        }
+        else {
+            setCaptionStyle("card__dsc text-white rounded-2xl p-5 flex flex-row justify-start items-end w-full h-full mt-20 z-50  text-2xl font-bold text-my_white");
+
+            setCardStyle("product__card w-full h-auto rounded-3xl flex flex-col items-center justify-center bg-orange-900 relative my-3 mx-3 ");
+        }
+
     }
 
-
     return (
-        <div className="product__card w-full h-auto rounded-3xl flex flex-col items-center justify-center bg-orange-900 relative my-3 mx-3 ">
+        <div className={cardStyle}>
             <img src={productImgUrl} className="flex  w-3/4 h-auto mt-5" />
             <div className={captionStyle}>{productDsc}</div>
-            <div onMouseEnter={()=>{hoverCard(true)}} onMouseLeave={()=>{hoverCard(false)}} className="shadow__layer absolute top-0 left-0 w-full h-full bg-my_dark_gray rounded-2xl opacity-40 hover:opacity-0 transition-opacity duration-500 "></div>
+            <div onMouseEnter={() => { hoverCard(true) }} onMouseLeave={() => { hoverCard(false) }} className="shadow__layer absolute top-0 left-0 w-full h-full bg-my_dark_gray rounded-2xl opacity-40 hover:opacity-0 transition-opacity duration-500 "></div>
         </div>
     );
 
