5137a51854ccd4323111d1a67b47856f3e3516ec
EmadNejati <emadnejati97@gmail.com>
2024-05-18 16:09:54 +0330

    TODAY RECOMMENDATION Responsive

---
+++ src/Components/Atoms/Recipe.js
@@ -0,0 +1,14 @@
+import "./Recipe.css"
+
+function Recipe({recipeTitle,recipe}){
+    return(
+        <div className="recipe__container">
+            
+            <div className="recipe__title text-2xl font-bold sm:text-4xl sm:font-extrabold sm:mb-3">{recipeTitle}</div>
+            <div className="recipe__text text-justify sm:text-lg ">{recipe}</div>
+        </div>
+    )
+
+}
+
+export default Recipe
